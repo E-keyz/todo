2c1e0a9c571656552124fdfd6a339447536633f0
Ekeyz <aladeemmanue@gmail.com>
2025-05-19 16:55:48 +0100

    Authentication added

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .import views
+from .views import task_list, complete_task, delete_task, register_user, login_view, logout_view
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
-    path('complete/<int:pk>/', views.complete_task, name='complete_task'),
-    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
+    path('', task_list, name='task_list'),
+    path('complete/<int:pk>/', complete_task, name='complete_task'),
+    path('delete/<int:pk>/', delete_task, name='delete_task'),
+    path('register/', register_user, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from tasks.models import Task
 
 # Create your views here.
+
+def register_user(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account Created Successfully")
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+    return render(request, 'tasks/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('task_list')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'tasks/login.html')
+    
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def task_list(request):
     if request.method == 'POST':
         title = request.POST.get('title')
